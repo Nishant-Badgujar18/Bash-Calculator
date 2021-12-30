ace474c09ca88c187753d5ee9ad2489514411011
Nishant Badgujar <67600360+Nishant-Badgujar18@users.noreply.github.com>
2021-12-30 14:48:27 +0530

    Add files via upload

---
+++ istack.h
@@ -0,0 +1,39 @@
+#ifndef ISTACK_H_INCLUDED
+#define ISTACK_H_INCLUDED
+
+#include "list.h"
+
+//  Stack for storing number list.
+typedef struct inode {
+	number *a;
+	struct inode *p;
+}inode;
+typedef inode *istack;
+void iinit(istack *s);
+
+void ipush(istack *s, number *a);
+
+number *ipop(istack *s);
+
+int iisempty(istack *s);
+
+
+//##############################################################################################
+
+// Stack for storing character Operators.
+typedef struct cnode {
+	char a;
+	struct cnode *p;
+}cnode;
+
+typedef cnode *cstack;
+void cinit(cstack *s);
+
+void cpush(cstack *s, char t);
+
+char cpop(cstack *s);
+
+int cisempty(cstack *s);
+
+
+#endif // ISTACK_H_INCLUDED
---
+++ list.h
@@ -0,0 +1,27 @@
+#ifndef LIST_H_INCLUDED
+#define LIST_H_INCLUDED
+
+#define PLUS 1
+#define MINUS -1
+
+typedef struct node {
+	int num;
+	struct node *next, *prev;
+}node;
+typedef struct number {
+	int sign, dec;
+	node *head, *tail;
+}number;
+
+void initNumber(number *a);
+void addDigit(number *a, char ch);
+void printNumber(number a);
+void destroyNumber(number *a);
+void appendleft(number *a, int no);
+int length(number a);
+void remov(number *a, int pos);
+
+void zeroRemov(number *a);
+
+
+#endif // LIST_H_INCLUDED
